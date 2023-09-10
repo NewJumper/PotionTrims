0dacf856b66243ead8963e24372634f2d51017fe
muhta <muhtasim.nabi@gmail.com>
2023-09-09 19:52:14 -0500

    player tick setup

--- src/main/java/com/newjumper/potiontrims/PotionTrims.java
+++ src/main/java/com/newjumper/potiontrims/PotionTrims.java
@@ -1,17 +1,29 @@
 package com.newjumper.potiontrims;
 
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(PotionTrims.MOD_ID)
 public class PotionTrims {
     public static final String MOD_ID = "potiontrims";
 
     public PotionTrims() {
-        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
         MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID)
+    public static class DeeperDarkerEvents {
+        @SubscribeEvent
+        public static void playerTick(final TickEvent.PlayerTickEvent event) {
+            for (ItemStack stack : event.player.getInventory().armor) {
+                if(stack.getItem() instanceof ArmorItem && stack.hasTag()) {
+                    System.out.println(stack.getTag());
+                }
+            }
+        }
     }
 }
